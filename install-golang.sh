36c2135f41ac08b86a43c47be4c523ebff594faf
Logan Brooke <logan.w.brooke@gmail.com>
2016-08-10 00:53:57 -0700

    export env in main process

--- install-golang.sh
+++ install-golang.sh
@@ -7,6 +7,7 @@ set -e
 echo "Installing go..."
 VERSION=1.6.2 && OS=linux && ARCH=amd64
 curl -O https://storage.googleapis.com/golang/go$VERSION.$OS-$ARCH.tar.gz
-tar -C /usr/local/bin -xzf go$VERSION.$OS-$ARCH.tar.gz
+tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
 rm go$VERSION.$OS-$ARCH.tar.gz
-export PATH=$PATH:/usr/local/bin/go/bin
+export PATH=$PATH:/usr/local/go/bin
+echo 'export PATH=$PATH:/usr/local/go/bin' >> /etc/profile
