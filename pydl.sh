ceb626f1015502caf7e36b27052f0301479aa6ca
Logan Brooke <logan.brooke@system1.com>
2017-06-26 15:46:58 -0700

    python version download/altinstall script

---
+++ pydl.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+
+declare -A VERSIONS=(
+    [3.6]=3.6.1
+    [3.5]=3.5.3
+    [3.4]=3.4.6
+    [2.7]=2.7.13
+)
+
+VERSION=${VERSIONS[${1:-"3.6"}]:-"3.6.1"}
+
+VERSION_DIR=${2:-"${HOME}/.python_versions/${VERSION}"}
+if [ -d ${VERSION_DIR} ]
+then
+    rm -rf ${VERSION_DIR}
+fi
+mkdir -p ${VERSION_DIR}
+
+DOWLOAD_URL="https://www.python.org/ftp/python/${VERSION}/Python-${VERSION}.tgz"
+
+curl -fO ${DOWLOAD_URL}
+tar -xvf "Python-${VERSION}.tgz"
+
+pushd "Python-${VERSION}"
+./configure --prefix ${VERSION_DIR}
+make
+make altinstall
+popd
+
+ln -s ${VERSION_DIR}/bin/python${1:-"3.6"} ${HOME}/bin/python${1:-"3.6"}_user
+
+rm -r Python-${VERSION}*
