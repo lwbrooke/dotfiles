8fab14b242a322f9caf190b63551adeec81b52e7
logan <logan.w.brooke@gmail.com>
2017-08-26 23:11:27 +0000

    install pydl requirements during python setup

--- install-python-3.sh
+++ install-python-3.sh
@@ -9,7 +9,15 @@ apt-get update -y
 apt-get install -y --no-install-recommends \
     python3-dev \
     python-dev \
-    python3-venv
+    libreadline-gplv2-dev \
+    libncursesw5-dev \
+    libssl-dev \
+    libsqlite3-dev \
+    tk-dev \
+    libgdbm-dev \
+    libc6-dev \
+    libbz2-dev \
+    checkinstall
 
 echo "Installing python packages..."
-pip install flake8 flake8-docstrings
+pip install flake8 flake8-docstrings virtualenv
